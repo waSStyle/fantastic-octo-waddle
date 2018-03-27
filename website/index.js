472e96158d384689cbce6347cdbccb6dd195274d
Sikksens <jovan.retief@gmail.com>
2018-03-27 16:14:52 +0200

    Update index.js

--- website/index.js
+++ website/index.js
@@ -181,13 +181,4 @@ module.exports = (bot, r) => {
 		if (error) throw error;
 		log("Website listening on port " + port + ".");
 	});
-
-	app.get("/", (request, response) => {
-		console.log(Date.now() + " Ping Received");
-		response.sendStatus(200);
-	  });
-	  app.listen(process.env.PORT);
-	  setInterval(() => {
-		http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-	  }, 280000);
 };
